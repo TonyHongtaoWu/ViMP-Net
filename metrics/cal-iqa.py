875118e5d53500fd8c22adc3e4e4940ea14aa380
HongtaoWu <141260395+TonyHongtaoWu@users.noreply.github.com>
2024-06-09 00:00:01 +0800

    Add files via upload

---
+++ metrics/cal-iqa.py
@@ -0,0 +1,58 @@
+import argparse
+import cv2
+import glob
+import os
+import torch
+import requests
+import numpy as np
+from os import path as osp
+from collections import OrderedDict
+from torch.utils.data import DataLoader
+import utils_image as util
+from rich.progress import track
+from natsort import natsorted
+import lpips
+
+def im2tensor(image, imtype=np.uint8, cent=1., factor=255./2.):
+    return torch.Tensor((image / factor - cent)
+                        [:, :, :, np.newaxis].transpose((3, 2, 0, 1)))
+
+
+path ="./result/"
+gt_path = "./testgt/"
+folders = os.listdir(path)
+print(path) 
+
+
+
+psnr = []
+ssim = []
+lpips_ = []
+loss_fn_alex = lpips.LPIPS(net='alex').cuda() 
+
+for folder in folders:
+    print(folder)
+    imgs = natsorted(glob.glob(osp.join(path, folder, '*.png')))
+    imgs_gt = natsorted(glob.glob(osp.join(gt_path, folder, '*.png')))
+    psnr_folder = []
+    ssim_folder = []
+    lpips_folder = []
+    
+    for i in track(range(len(imgs))):
+        output = cv2.imread(imgs[i])
+        gt = cv2.imread(imgs_gt[i])
+        if output.shape != gt.shape:
+            print(output.shape, gt.shape)
+        psnr_folder.append(util.calculate_psnr(output, gt))
+        ssim_folder.append(util.calculate_ssim(output, gt))
+        lpips_folder.append(loss_fn_alex(im2tensor(output).cuda(), im2tensor(gt).cuda()).item())
+
+    psnr.append(np.mean(psnr_folder))
+    ssim.append(np.mean(ssim_folder))
+    lpips_.append(np.mean(lpips_folder))
+    
+print('psnr: ', np.mean(psnr))
+print('ssim: ', np.mean(ssim))
+print('lpips: ', np.mean(lpips_))
+  
+    
